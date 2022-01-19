0bf797a6768e7a6027f2ec0444d56c363c2aad53
Latif Essam <latifekhaliel@gmail.com>
2022-01-19 10:22:19 +0200

    commiting to start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>first Things First</h1>
+    </div>
+  );
+}
+
+export default App;
