70666af296e686a5994c4496cedbfabebfe2a02f
Latif Essam <latifekhaliel@gmail.com>
2022-01-20 10:28:09 +0200

    starting the time-tracking-dashboard bransh project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./styles/App.css";
 
 function App() {
   return (
-    <div>
-      <h1>first Things First</h1>
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ fontSize: 25, color: "blue" }}>Hello Winter</h1>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+        <p key={i}>🔻</p>
+      ))}
+      <h2>Goodbye Winter 🚀🏔🗻🌨🌝 </h2>
     </div>
   );
 }
